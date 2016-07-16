7357919bbf04c03488252fdfe4cdf11aa9e7d633
Hector Huertas <hectorhuertas@gmail.com>
2016-07-16 21:11:34 +0200

    Enemy generation

---
+++ Assets/Scripts/GenerateEnemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateEnemy : MonoBehaviour {
+
+	public GameObject angel;
+	private float limit;
+
+	// Use this for initialization
+	void Start () {
+		limit = 98f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int XPosition = Random.Range (-6, 6);
+		int dice = Random.Range (0, 100);
+		bool hit = dice > limit ? true : false;
+
+		if (hit) {
+			createAngel(new Vector3(XPosition, 5.5f, 0));
+			limit -= 0.2f;
+		}
+	}
+
+	void createAngel(Vector3 position) {
+		Instantiate(angel, position, Quaternion.Euler(0,0,0));
+	}
+
+
+}
